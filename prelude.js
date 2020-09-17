8c3375c13dece216b07d2aae6950c868c8be4604
Keita Amadou <amadou.keita@etudiant.univ-reims.fr>
2020-09-17 11:29:58 +0200

    Début de l'exercice 10

--- prelude.js
+++ prelude.js
@@ -16,7 +16,8 @@ function lowerCase(str = '') {
 }
 console.log('---- Exercice 1 ----');
 console.log(lowerCase('Chopper'));
-console.log(lowerCase('TONY TONY \n'));
+console.log(lowerCase('TONY TONY'));
+console.log("\n");
 
 //Exercice 2
 function upperCase(upper = ''){
@@ -36,7 +37,8 @@ function upperCase(upper = ''){
 }
 console.log("---- Exerice 2 ----");
 console.log(upperCase('Chopper'));
-console.log(upperCase('TONY TONY \n'));
+console.log(upperCase('TONY TONY'));
+console.log("");
 
 //Exercice 3
 function length(length='')
@@ -49,7 +51,8 @@ function length(length='')
 	return count;
 }
 console.log("---- Exerice 3 ----");
-console.log(length('Ch0pper\n'));
+console.log(length('Ch0pper'));
+console.log("");
 
 //Exercice 4
 function capitalize(cap='', restToLoweropt = false){
@@ -81,8 +84,9 @@ function capitalize(cap='', restToLoweropt = false){
 	return sentence;
 }
 console.log("---- Exerice 4 ----");
-console.log(capitalize('one COde', false));
-console.log(capitalize('One Code \n', true));
+console.log(capitalize('one Code'));
+console.log(capitalize('One CODE', true));
+console.log("");
 
 //Exercice 5
 function decapitalize(decap='')
@@ -91,11 +95,11 @@ function decapitalize(decap='')
 	for (let i = 0; i < decap.length; i++)
 	{
 		const code = decap.charCodeAt(i);
-		if ((i == 0 && code >= 65 && code <= 90) || decap.charCodeAt(-i) == 32)
+		if ((i == 0 && code >= 65 && code <= 90) || decap.charCodeAt(i - 1) == 32)
 		{
 			sentence += String.fromCharCode(code + 32);
 		}
-		else if ((i == 0 && code >= 97 && code <= 122) || decap.charCodeAt(-i) == 32) {
+		else if ((i == 0 && code >= 97 && code <= 122) || decap.charCodeAt(i - 1) == 32) {
 			sentence += String.fromCharCode(code);
 		}
 		else if (i != 0) {
@@ -107,9 +111,133 @@ function decapitalize(decap='')
 console.log("---- Exerice 5 ----");
 console.log(decapitalize('El Drago'));
 console.log(decapitalize('Wooman'));
+console.log("");
 
 //Exercice 6
-/*function kebabCase(kebab = '')
+function kebabCase(kebab = '')
 {
+	let sentence = '';
+	let tiretDebFin = false;
+	for (let i = 0; i < kebab.length; i++)
+	{
+		tiretDebFin = false;
+		const code = kebab.charCodeAt(i);
+		if (code == 32)
+		{
+			sentence += String.fromCharCode(code + 13);	
+		}
+		if (i == 0 && code == 45 || i == kebab.length-1 && code == 45)
+		{
+			tiretDebFin = true;
+		}
+		if (code >= 65 && code <= 90)
+		{
+			if (kebab.charCodeAt(i-1) != 45 && i != 0)
+			{
+				sentence += "-" + String.fromCharCode(code+32);
+			}
+			else
+			{
+				sentence += String.fromCharCode(code+32);
+			}
+		}
+		else if (code != 32 && tiretDebFin == false)
+		{
+			sentence += String.fromCharCode(code);
+		}		
+	}
+	return sentence;
+}
+console.log("---- Exerice 6 ----");
+console.log(kebabCase('love one code'));
+console.log(kebabCase('LoveOneCode'));
+console.log(kebabCase('-Love-One-Code-'));
+console.log("");
 
-}*/
+//Exercice 7
+function snakeCase(snake = '')
+{
+	let sentence = '';
+	let tiretDebFin = false;
+	for (let i = 0; i < snake.length; i++)
+	{
+		tiretDebFin = false;
+		const code = snake.charCodeAt(i);
+		if (code == 32)
+		{
+			sentence += String.fromCharCode(code + 63);	
+		}
+		if (i == 0 && code == 45 || i == snake.length-1 && code == 45)
+		{
+			tiretDebFin = true;
+		}
+		if (code >= 65 && code <= 90)
+		{
+			if (snake.charCodeAt(i-1) != 45 && i != 0)
+			{
+				sentence += "_" + String.fromCharCode(code+32);
+			}
+			else
+			{
+				sentence += String.fromCharCode(code+32);
+			}
+		}
+		else if (code != 32 && tiretDebFin == false)
+		{
+			if (code == 45)
+			{
+				sentence += String.fromCharCode(code + 50);
+			}
+			else
+			{
+				sentence += String.fromCharCode(code);
+			}
+		}		
+	}
+	return sentence;
+}
+console.log("---- Exerice 7 ----");
+console.log(snakeCase('gold d roger'));
+console.log(snakeCase('GoldDRoger'));
+console.log(snakeCase('-Gold-D-Roger-'));
+console.log("");
+
+//Exercice 8
+function swapCase(swap=''){
+	let sentence = '';
+	for (let i = 0; i < swap.length; i++)
+	{
+		const code = swap.charCodeAt(i);
+
+		if (code >= 97 && code <= 122)
+		{
+			sentence += String.fromCharCode(code - 32);
+		}
+		if (code >= 65 && code <= 90) {
+			sentence += String.fromCharCode(code + 32);
+		}
+		else if (code >= 48 && code <= 57 || code == 32) {
+			sentence += String.fromCharCode(code);
+		}
+	}
+	return sentence;
+}
+console.log("---- Exerice 8 ----");
+console.log(swapCase('One Code'));
+console.log(swapCase('1337 Fruits'));
+console.log("");
+
+//Exerxice 9
+function charAt(char='', position)
+{
+	let sentence = '';
+	for (let i = 0; i < char.length; i++)
+	{
+		sentence = char[position];
+	}
+	return sentence;
+}
+console.log("---- Exerice 9 ----");
+console.log(charAt('chopper', 0));
+console.log(charAt('chopper', 1));
+console.log("");
